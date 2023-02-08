dc1482b8ee34b9f199ec1dbe9fc12f268b05ff51
Joab Almeida <joabalmeida.dev@gmail.com>
2023-02-07 22:43:53 -0300

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: 'airbnb-base',
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'no-console': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    'class-methods-use-this': 'off',
+    'linebreak-style': 'off',
+
+  },
+};
