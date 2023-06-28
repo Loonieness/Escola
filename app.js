d9c3678939fc9f53925f87e12f1b965f8fc8b56e
Joab Almeida <joabalmeida.dev@gmail.com>
2023-06-27 23:53:28 -0300

    iniciando o multer para upload de foto

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ import homeRoutes from './src/routes/homeRoutes';// home minusculo pois está in
 import userRoutes from './src/routes/userRoutes';
 import tokenRoutes from './src/routes/tokenRoutes';
 import alunoRoutes from './src/routes/alunoRoutes';
+import fotoRoutes from './src/routes/fotoRoutes';
 
 class App {
   constructor() {
@@ -27,6 +28,7 @@ class App {
     this.app.use('/users/', userRoutes);// tudo o que for /user/, é tratado pelo userRoutes
     this.app.use('/tokens/', tokenRoutes);
     this.app.use('/alunos/', alunoRoutes);
+    this.app.use('/fotos/', fotoRoutes);
   }
 }
 
