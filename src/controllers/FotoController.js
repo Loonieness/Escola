23b1328441bd62b5e5ed8e84199b25008d592e72
Joab Almeida <joabalmeida.dev@gmail.com>
2023-06-28 23:27:58 -0300

    tratamento de imagem para só vir PNG e JPEG

--- src/controllers/FotoController.js
+++ src/controllers/FotoController.js
@@ -1,6 +1,19 @@
+import multer from 'multer';
+import multerConfig from '../config/multerConfig';
+
+const upload = multer(multerConfig).single('foto');
+
 class FotoController {
   async store(req, res) {
-    res.json(req.file);
+    return upload(req, res, (error) => { // se essa seta dá erro, é por que falta um return
+      if (error) {
+        return res.status(400).json({
+          errors: [error.code],
+        });
+      }
+
+      return res.json(req.file);
+    });
   }
 }
 
